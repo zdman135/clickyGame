eee47a2c674e477fb5771081b7c5e19eeb12a477
Rich Jen <rich.rjen@gmail.com>
2019-04-07 00:22:11 -0400

    Modified Data
    changed image path to
    include trailing / in url

--- src/data/data.js
+++ src/data/data.js
@@ -1,51 +1,51 @@
 const models = [
   {
     id: '1',
-    image: 'images/model1.jpg'
+    image: '/images/model1.jpg'
   },
   {
     id: '2',
-    image: 'images/model2.jpg'
+    image: '/images/model2.jpg'
   },
   {
     id: '3',
-    image: 'images/model3.jpg'
+    image: '/images/model3.jpg'
   },
   {
     id: '4',
-    image: 'images/model4.jpg'
+    image: '/images/model4.jpg'
   },
   {
     id: '5',
-    image: 'images/model5.jpg'
+    image: '/images/model5.jpg'
   },
   {
     id: '6',
-    image: 'images/model6.jpg'
+    image: '/images/model6.jpg'
   },
   {
     id: '7',
-    image: 'images/model7.jpg'
+    image: '/images/model7.jpg'
   },
   {
     id: '8',
-    image: 'images/model8.jpg'
+    image: '/images/model8.jpg'
   },
   {
     id: '9',
-    image: 'images/model9.jpg'
+    image: '/images/model9.jpg'
   },
   {
     id: '10',
-    image: 'images/model10.jpg'
+    image: '/images/model10.jpg'
   },
   {
     id: '11',
-    image: 'images/model11.jpg'
+    image: '/images/model11.jpg'
   },
   {
     id: '12',
-    image: 'images/model12.jpg'
+    image: '/images/model12.jpg'
   },
 ]
 
