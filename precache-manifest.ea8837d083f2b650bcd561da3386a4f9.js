2d3e1f0d42fa2c5ca1b3b096518edc16d0205d9f
Rich Jen <rich.rjen@gmail.com>
2019-04-07 00:18:00 -0400

    Updates

---
+++ precache-manifest.ea8837d083f2b650bcd561da3386a4f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "538d9c8aebefa50a71c3",
+    "url": "/clickyGame/static/css/main.781eb944.chunk.css"
+  },
+  {
+    "revision": "538d9c8aebefa50a71c3",
+    "url": "/clickyGame/static/js/main.1a676307.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e978b177f15b874b5b3f596942c71b13",
+    "url": "/clickyGame/index.html"
+  }
+];
